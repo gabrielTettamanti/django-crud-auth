dac4d06f86fba5e37f0d3859cda44d47aa1e2e98
gabrielTettamanti <gabrieltettamanti.dev@gmail.com>
2023-03-27 10:16:58 -0300

    Some changes

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,5 +20,8 @@ from tasks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('tasks/', views.tasks, name='tasks'),
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('logout/', views.signout, name='logout'),
 ]
